97050265891bea8c881ebd8f7923aa02633df8fb
leuthier <leuthier@users.noreply.github.com>
2023-05-14 05:26:48 -0300

    0.2.2 - Icones dinamicos, Agenda 100% dinamica

--- background.js
+++ background.js
@@ -1,40 +1,92 @@
-function getNextGame() {
-  document.addEventListener('DOMContentLoaded', function() {
-    var gameInfoElement = document.getElementById('game-info');
-    const url = 'https://www.placardefutebol.com.br/time/sport/proximos-jogos';
-    const xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        const html = xhr.responseText;
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-
-        const dataElement = doc.querySelector('.match__lg_card--datetime');
-        const leagueElement = doc.querySelector('.match__lg_card--league');
-        const teamsElement = doc.querySelectorAll('.match__lg_card--ht-name.text');
-
-        const data = dataElement ? dataElement.textContent.trim().split('\n').map(item => item.trim()).filter(item => item !== '') : 'Data não encontrada';
-        const liga = leagueElement ? leagueElement.textContent.trim() : 'Liga não encontrada';
-        const times = Array.from(teamsElement).map(element => element.textContent);
-
-        var info = liga + "<br>" + times[0] + " VS " + times[1] + "<br>" ;
-        var data_pretty = data[0] + "às" + data[1];
-        gameInfoElement.innerText = info + data_pretty;
-      }
-    };
-
-    xhr.open('GET', url);
-    xhr.send();
-
-/*     var gameInfo = 'Próximo jogo: ' + timeMandante + ' vs. ' + timeVisitante + ' - Data: ' + dataPartida;
-    console.log('gameInfo' + gameInfo);
-
-    
-    */
-  
-  });
-    
+function getRequest(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(xhr.responseText);
+    }
+  };
+  xhr.send();
 }
 
-getNextGame();
+function parseHTML(html) {
+  var parser = new DOMParser();
+  return parser.parseFromString(html, 'text/html');
+}
+
+var url = 'https://www.placardefutebol.com.br/time/sport/proximos-jogos';
+getRequest(url, function (responseText) {
+  var html = parseHTML(responseText);
+  var data_elements = html.querySelectorAll('.match__lg_card--datetime');
+  var datas = Array.from(data_elements).map(function (element) {
+    return element.textContent.trim().split(' ') || 'Data não encontrada';
+  });
+
+  var league_elements = html.querySelectorAll('.match__lg_card--league');
+  var campeonato = Array.from(league_elements).map(function (element) {
+    return element.textContent.trim() || 'Liga não encontrada';
+  });
+
+  var home_teams_elements = html.querySelectorAll('.match__lg_card--ht-name.text');
+  var away_teams_elements = html.querySelectorAll('.match__lg_card--at-name.text');
+  var team_home = Array.from(home_teams_elements).map(function (element) {
+    return element.textContent.trim() || 'Time da casa não encontrado';
+  });
+  var team_away = Array.from(away_teams_elements).map(function (element) {
+    return element.textContent.trim() || 'Time visitante não encontrado';
+  });
+
+  var src_home_elements = html.querySelectorAll('.match__lg_card--ht-logo');
+  var img_src_home = Array.from(src_home_elements).map(function (element) {
+    return element.querySelector('img').getAttribute('src');
+  });
+
+  var src_away_elements = html.querySelectorAll('.match__lg_card--at-logo');
+  var img_src_away = Array.from(src_away_elements).map(function (element) {
+    return element.querySelector('img').getAttribute('src');
+  });
+
+  var gamesList = document.getElementById('games-list');
+
+  for (var i = 0; i < home_teams_elements.length; i++) {
+    var gameContainer = document.createElement('div');
+    gameContainer.className = 'game';
+
+    var homeIcon = document.createElement('img');
+    homeIcon.className = 'team-icon';
+    homeIcon.src = img_src_home[i];
+
+    var awayIcon = document.createElement('img');
+    awayIcon.className = 'team-icon';
+    awayIcon.src = img_src_away[i];
+
+    var gameInfo = document.createElement('div');
+    gameInfo.className = 'game-info';
+
+    var gameLeague = document.createElement('span');
+    gameLeague.textContent = campeonato[i];
+    gameLeague.style.fontWeight = 'bold';
+
+    var gameTeams = document.createElement('span');
+    gameTeams.textContent = team_home[i] + ' vs ' + team_away[i];
+
+    var gameDate = document.createElement('span');
+    gameDate.className = 'game-date';
+
+    if (datas[i].length === 2) {
+      gameDate.textContent = datas[i][0].charAt(0).toUpperCase() + datas[i][0].slice(1) + ' às ' + datas[i][datas[i].length - 1];
+    } else {
+      gameDate.textContent = datas[i][0].charAt(0).toUpperCase() + datas[i][0].slice(1) + ' ' + datas[i][1] + ' às ' + datas[i][datas[i].length - 1];
+    }
+
+    gameInfo.appendChild(gameLeague);
+    gameInfo.appendChild(gameTeams);
+    gameInfo.appendChild(gameDate);
+    
+    gameContainer.appendChild(homeIcon);
+    gameContainer.appendChild(awayIcon);
+    gameContainer.appendChild(gameInfo);
+    
+    gamesList.appendChild(gameContainer);
+  }
+});
